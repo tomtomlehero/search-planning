4ef34a5a844d82b5d79ccca9c929f2f14aba9d44
Mathieu Lanfranchi <mathieu.lanfranchi@acensi.fr>
2024-11-20 14:50:42 +0100

    CSP / Forward checking

--- src/main/java/fr/mla/csp/CSP.java
+++ src/main/java/fr/mla/csp/CSP.java
@@ -7,19 +7,19 @@ import java.util.Map;
 import java.util.Set;
 
 import fr.mla.NoSolutionException;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public abstract class CSP<V extends Variable<?, U>, U extends Value<?>> {
+
+  private long visitedNodes = 0;
 
   protected Set<V> variables;
 
   public Map<V, U> backtrackingSearch() throws NoSolutionException {
-    return backtrackingSearch(new HashMap<>(), getInitialDomains());
-  }
-
-  private Map<V, Set<U>> getInitialDomains() {
-    Map<V, Set<U>> domains = new HashMap<>();
-    variables.forEach(v -> domains.put(v, v.getInitialDomainValues()));
-    return domains;
+    Map<V, U> assignments = backtrackingSearch(new HashMap<>(), getInitialDomains());
+    log.info("Visited nodes: {}", visitedNodes);
+    return assignments;
   }
 
   private Map<V, U> backtrackingSearch(Map<V, U> assignment, Map<V, Set<U>> domains) throws NoSolutionException {
@@ -28,13 +28,19 @@ public abstract class CSP<V extends Variable<?, U>, U extends Value<?>> {
     }
     V variable = selectUnassigned(assignment, domains);
     for (U value : domains.get(variable)) {
+      visitedNodes++;
       if (isConsistent(variable, value, assignment)) {
         assignment.put(variable, value);
         try {
           Map<V, Set<U>> domainsLookAhead = lookAhead(assignment, domains, variable, value);
-          if (allDomainsNotEmpty(domainsLookAhead)) {
-            return backtrackingSearch(assignment, domainsLookAhead);
+//          log.info("Assignment {}", assignment);
+//          log.info("Assigning {} := {}", variable, value);
+//          log.info("Domain after {}", domainsLookAhead);
+          if (anyDomainsEmpty(domainsLookAhead)) {
+            continue;
           }
+          return backtrackingSearch(assignment, domainsLookAhead);
+
         } catch (NoSolutionException e) {
           assignment.remove(variable);
         }
@@ -43,18 +49,20 @@ public abstract class CSP<V extends Variable<?, U>, U extends Value<?>> {
     throw new NoSolutionException();
   }
 
-  private boolean allDomainsNotEmpty(Map<V, Set<U>> domains) {
-    return domains.values().stream().noneMatch(Set::isEmpty);
+  private boolean anyDomainsEmpty(Map<V, Set<U>> domains) {
+    return domains.values().stream().anyMatch(Set::isEmpty);
   }
 
   private Map<V, Set<U>> lookAhead(Map<V, U> assignment, Map<V, Set<U>> domains, V variable, U value) {
     Map<V, Set<U>> domainsAhead = new HashMap<>();
     domains.forEach((v, values) -> {
       Set<U> prunedDomain = new HashSet<>(values);
-      if (v.get() == variable.get()) {
-        prunedDomain.removeIf(u -> !u.get().equals(value.get()));
-      } else {
-        prunedDomain.removeIf(u -> !isConsistent(v, u, assignment));
+      if (!assignment.containsKey(v)) {
+        if (v.get() == variable.get()) {
+          prunedDomain.removeIf(u -> !u.get().equals(value.get()));
+        } else {
+          prunedDomain.removeIf(u -> !isConsistent(v, u, assignment));
+        }
       }
       domainsAhead.put(v, prunedDomain);
     });
@@ -65,7 +73,14 @@ public abstract class CSP<V extends Variable<?, U>, U extends Value<?>> {
     return assignment.size() == variables.size();
   }
 
-  protected V selectUnassigned(Map<V, U> assignment, Map<V, Set<U>> domains) {
+
+  private Map<V, Set<U>> getInitialDomains() {
+    Map<V, Set<U>> domains = new HashMap<>();
+    variables.forEach(v -> domains.put(v, v.getInitialDomainValues()));
+    return domains;
+  }
+
+  private V selectUnassigned(Map<V, U> assignment, Map<V, Set<U>> domains) {
     return variables.stream()
         .filter(v -> !assignment.containsKey(v))
         .min(Comparator.comparingInt(o -> domains.get(o).size()))
--- src/main/java/fr/mla/csp/problem/NQueen.java
+++ src/main/java/fr/mla/csp/problem/NQueen.java
@@ -14,7 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class NQueen extends CSP<NQueenVariable, NQueenValue> {
 
-  static final int N = 4;
+  static final int N = 100;
 
   public NQueen() {
     Set<NQueenVariable> variables = new HashSet<>();
